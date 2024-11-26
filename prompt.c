4e3dbf048cdaea834c2ff2b4b532dcef8ceda0bf
Al3xM4rki <alexmarqui11@gmail.com>
2024-11-26 20:23:16 +0100

    prompt

--- prompt.c
+++ prompt.c
@@ -17,7 +17,8 @@ int main(){
         //ejemplo de entrada
         char comando[10];
         scanf("%s", comando);
-        printf("%s\n", comando);
+        printf("\n%s\n", comando);
     }
+
 
 }
